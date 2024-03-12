176325f3d646047490d4698282b6b8eb53bccc2a
Mica-Pimentel <72927832+Mica-Pimentel@users.noreply.github.com>
2024-03-12 10:21:52 -0300

    11_03_24
    criaçao do arquivo referente a aula do dia 11-03-24. adicionando uma cauculadora bem simplees capaz de realizar as 4 operações(+-*/)

---
+++ 1Semestre/PSC/maiara/11_03_24/src/pkg11_03_24/Main.java
@@ -0,0 +1,34 @@
+package pkg11_03_24;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+     
+        Scanner ler = new Scanner(System.in);
+        
+        System.out.println("calculadora simples\n");
+        System.out.println("INSIRA O PRIMEIRO NUMERO");
+        int a = ler.nextInt();
+        System.out.println("INSIRA O SEGUNDO NUMERO");
+        int b = ler.nextInt();
+        
+        System.out.println(
+                "RESULTADOS"+
+        "\nA soma de: "+ a +" + "+ b+" = "+(a+b) +
+        "\nA subtracao de:" + a +" - "+ b+" = "+(a-b)+
+        "\nA multiplicacao de:" + a +" x "+ b+" = "+(a*b)+
+        "\nA divisao inteira de:" + a +" / "+ b+" = "+(a/b)+
+        "\nA divisao exata de:" + a +" /"+ b+" = "+((float)a/b)
+              
+
+        
+        );
+        
+        
+        
+        
+    }
+    
+}
