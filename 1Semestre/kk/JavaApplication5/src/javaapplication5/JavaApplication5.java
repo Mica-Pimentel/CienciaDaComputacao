fd29e4dbad6a05902f55494357cdf6c79855612d
Mica-Pimentel <72927832+Mica-Pimentel@users.noreply.github.com>
2024-03-19 13:32:12 -0300

    ,

---
+++ 1Semestre/kk/JavaApplication5/src/javaapplication5/JavaApplication5.java
@@ -0,0 +1,19 @@
+package javaapplication5;
+
+import java.util.Scanner;
+
+public class JavaApplication5 {
+    public static void main(String[] args) {
+      //dado um numero inteiro obter o ultimo algarimos desse numero.
+      
+        Scanner ler = new Scanner(System.in);
+      
+        
+        int n = ler.nextInt();
+        float resultado = n/10;
+        
+        System.out.println(resultado);
+        
+    }
+    
+}
