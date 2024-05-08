e80ee0240c85a375c27e41553a1940b2a43bf600
Micael Pimentel <72927832+Mica-Pimentel@users.noreply.github.com>
2024-05-07 21:24:30 -0300

    ...
    ....

---
+++ 1Semestre/PSC/teorica_marivaldo/Aula07_05_24/src/aula07_05_24/Aula07_05_24.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula07_05_24;
+
+/**
+ *
+ * @author 722414878
+ */
+public class Aula07_05_24 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
